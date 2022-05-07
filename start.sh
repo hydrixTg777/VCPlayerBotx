b08d77c6a91922c868e6dc742d72228bdb8f78ad
HyDrix <87888078+Hydrayt777@users.noreply.github.com>
2022-05-07 14:37:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/subinps/VCPlayerBot /VCPlayerBot
+  git clone https://github.com/Hydrayt777/VCPlayerBotx /VCPlayerBotx
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/subinps/VCPlayerBot -b $BRANCH /VCPlayerBot
+  git clone https://github.com/Hydrayt777/VCPlayerBotx -b $BRANCH /VCPlayerBotx
 fi
-cd /VCPlayerBot
+cd /VCPlayerBotx
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
